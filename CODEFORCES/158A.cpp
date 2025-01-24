1a42c03810812ebb19957412003ddf2c8b7f5177
ahmed-farhanur-rashid <ahmed.farhanur.rashid@gmail.com>
2025-01-25 00:04:55 +0600

    Solution to 158A.cpp

---
+++ CODEFORCES/158A.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    int k;
+    int counter = 0;
+
+    cin >> n >> k;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] >= arr[k - 1] && arr[i] > 0)
+            counter++;
+    }
+
+    cout << counter << endl;
+
+    return 0;
+}
