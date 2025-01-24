e64749cf7984347ec715bd5efae1d369bb660a77
ahmed-farhanur-rashid <ahmed.farhanur.rashid@gmail.com>
2025-01-25 00:02:10 +0600

    Solution to 1180A

---
+++ CODEFORCES/1180A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cin >> n;
+
+    // Formula from geeks for geeks
+    cout << (n*n) + (n-1)*(n-1) << endl;
+
+    return 0;
+}
